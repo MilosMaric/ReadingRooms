0d7da1e577c70619355c12d64540da7b63bdcf11
Miloš Marić <milososig AT gmail DOT com>
2016-08-26 22:29:05 +0200

    Implementirana registracija korisnika.

--- API/Scripts/controllers/registerController.js
+++ API/Scripts/controllers/registerController.js
@@ -1,9 +1,70 @@
-app.controller('registerController', ['$scope', '$window', 'userService', function($scope, $window, userService){
+app.controller('registerController', ['$scope', '$window', 'userService', 'facultyService',
+                function($scope, $window, userService, facultyService){
 
     function init() {
       $scope.$parent.checkSession();
+      $scope.user = {};
+      $scope.isOccupied = false;
+      $scope.getAllUsers();
+      facultyService.getAll().then(
+        function(response) {
+          if(response.data) {
+            $scope.faculties = response.data;
+          }
+        }
+      );
+    }
+
+    $scope.getAllUsers = function() {
+      userService.getAll().then(
+        function(response) {
+          if(response.data) {
+            $scope.users = response.data;
+          }
+        }
+      );
     }
 
     init();
+
+    $scope.checkUsername = function() {
+      if($scope.users && $scope.users.length && $scope.user.Username && $scope.user.Username.trim().length > 0) {
+        for(var i = 0; i < $scope.users.length; i++) {
+          if($scope.users[i].Username === $scope.user.Username) {
+            $scope.isOccupied = true;
+            return true;
+          }
+        }
+      }
+      $scope.isOccupied = false;
+      return false;
+    }
+
+    $scope.register = function() {
+      if($scope.isFormValid()) {
+        $scope.user.Role = "Student";
+        userService.add($scope.user).then(
+          function(response) {
+            if(response.data) {
+              alert("Registracija je uspešna. Bićete prebačeni na stranicu za prijavljivanje.");
+              $window.location.href = "#/login";
+            } else {
+              alert("Došlo je do greške prilikom registracije.");
+            }
+          }
+        );
+      }
+    }
+
+    $scope.isFormValid = function() {
+      var user = $scope.user;
+      if(!$scope.checkUsername() && user.Password && user.Password.trim().length > 0 && user.FacultyId &&
+          user.Index && user.Index.trim().length > 0) {
+        return true;
+      }
+
+      alert("Sva polja moraju biti popunjena i korisničko ime jedinstveno. Nije jedinstveno ako je polje za unos uokvireno crveno!");
+      return false;
+    }
 
 }]);
--- API/Scripts/services/userService.js
+++ API/Scripts/services/userService.js
@@ -1,9 +1,6 @@
 app.service('userService', function($http){
 	var url = '/api/user';
-	this.register = function(user){
-		return $http.post(url + "/register", user);
-	}
-
+	
 	this.login = function(user){
 		return $http.post("api/login", user);
 	}
